cbbc29e1d89942fa8aa0d3f0f49594bd4b623a3e
ErikHartman <erik.hartman@hotmail.com>
2023-12-30 13:48:41 +0100

    Got disassembly index working

--- src/disassembly/disassembly.py
+++ src/disassembly/disassembly.py
@@ -20,16 +20,26 @@ def get_disassembly(P: dict, disassembly_indexes: dict):
 
 
 def get_disassembly_indexes(G: nx.DiGraph):
-    # Normalize node outputs
+    """
+    The input is a weighted graph 
+
+    sequence -w-> subsequence
+
+    When we caluclate disassembly index we want to know:
+
+    Given that we have a sequence:
+        What is the estimated length to get there
+
+    """
+    # Normalize node outputs to make them into probabilites
+    G.remove_edges_from(nx.selfloop_edges(G))
     for node in G.nodes():
-        out_edges = G.out_edges(node, data=True)
-        total_out = sum([data["weight"] for s, t, data in out_edges])
-        for key, target, data in out_edges:
+        in_edges = G.in_edges(node, data=True)
+        total_in = sum([data["weight"] for s, t, data in in_edges])
+        for key, target, data in in_edges:
             nx.set_edge_attributes(
-                G, {(key, target): {"weight": data["weight"] / total_out}}
+                G, {(key, target): {"weight": data["weight"] / total_in}}
             )
-
-
     disassembly_indexes = {sequence: 0 for sequence in G.nodes()}
     for i, (sequence, _) in enumerate(disassembly_indexes.items()):
         longest_object = sorted(list(disassembly_indexes.keys()), key=len)[-1]
@@ -37,7 +47,7 @@ def get_disassembly_indexes(G: nx.DiGraph):
             continue
         paths_from_sequence_to_longest_object = list(
             nx.all_simple_paths(G.reverse(), sequence, longest_object)
-        ) # This is slow as hell for many paths
+        )  # This is slow as hell for many paths
 
         print(
             f"\r {i} / {len(disassembly_indexes.keys())} | checking {len(paths_from_sequence_to_longest_object)} path",
@@ -49,9 +59,6 @@ def get_disassembly_indexes(G: nx.DiGraph):
             prob = 1
             length = len(path) - 1
             for i in range(len(path) - 1):
-                if prob <= 0.001:
-                    prob = 0
-                    break
                 prob *= G.get_edge_data(path[i + 1], path[i])["weight"] / sum(
                     [data["weight"] for _, _, data in G.in_edges(path[i], data=True)]
                 )
