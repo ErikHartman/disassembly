0f4258db6d6f59ef68f6d5df97728abc4b03765a
ErikHartman <erik.hartman@hotmail.com>
2024-01-05 15:13:48 +0100

    Added MC estimation of DI

---
+++ paper/main.md
@@ -0,0 +1,1 @@
+# Estimating disassembly indexes in disassembling systems
--- src/disassembly/disassembly.py
+++ src/disassembly/disassembly.py
@@ -1,6 +1,22 @@
 import networkx as nx
 import math
 
+"""
+
+TODO: MC approximation of DI
+
+
+Remove self loops.
+
+release N particles
+
+for the traversal of each node. 
+
+Note how long it took to get there.
+The end is the average length
+
+"""
+
 
 def get_disassembly(P: dict, disassembly_indexes: dict):
     """
@@ -82,69 +98,44 @@ def get_disassembly_indexes(G: nx.DiGraph, min_weight : float = 0.01):
     print(f"\n Averaged DI: {sum(disassembly_indexes.values()) / len(disassembly_indexes.keys()):.2f}")
     return disassembly_indexes
 
-def get_shortest_disassembly_indexes(G: nx.DiGraph):
-    G = G.reverse()
-    disassembly_indexes = {sequence: 0 for sequence in G.nodes()}
-    for i, (sequence, _) in enumerate(disassembly_indexes.items()):
-        longest_object = sorted(list(disassembly_indexes.keys()), key=len)[-1]
-        if sequence == longest_object:
-            continue
-        disassembly_indexes[sequence] = nx.shortest_path_length(G, sequence, longest_object)
+import numpy as np
+def get_disassembly_indexes_mc(G: nx.DiGraph, N_particles : int):
+    """
+    The input is a weighted graph and a min_weight threshold.
 
-    print(f"\n Averaged DI: {sum(disassembly_indexes.values()) / len(disassembly_indexes.keys()):.2f}")
-    return disassembly_indexes
+    Edges with weight below min weight are removed.
 
-def get_most_probable_disassembly_indexes(G: nx.DiGraph, min_weight : float = 0.01):
+    sequence -w-> subsequence
 
+    When we caluclate disassembly index we want to know:
+
+    Given that we have a sequence:
+        What is the estimated length to get there
+
+    """
     # Normalize node outputs to make them into probabilites
     G.remove_edges_from(nx.selfloop_edges(G))
-    for node in G.nodes():
-        in_edges = G.in_edges(node, data=True)
-        total_in = sum([data["weight"] for s, t, data in in_edges])
-        for key, target, data in in_edges:
-            nx.set_edge_attributes(
-                G, {(key, target): {"weight": data["weight"] / total_in}}
-            )
-
-    # Remove very improbable edges
-    no_0_G = nx.DiGraph()
-    for source, target, data in G.edges(data=True):
-        if data["weight"] > min_weight:
-            no_0_G.add_edge(source, target, **data)
-    print(f"Removing low prob. (<{min_weight}) edges. Before: {G.number_of_edges()} | after: {no_0_G.number_of_edges()}")
-    G = no_0_G
     
-    disassembly_indexes = {sequence: 0 for sequence in G.nodes()}
-    for i, (sequence, _) in enumerate(disassembly_indexes.items()):
-        longest_object = sorted(list(disassembly_indexes.keys()), key=len)[-1]
-        if sequence == longest_object:
-            continue
-        paths_from_sequence_to_longest_object = list(
-            nx.all_simple_paths(G.reverse(), sequence, longest_object)
-        )  # This is slow as hell for many paths
-
-        print(
-            f"\r {i} / {len(disassembly_indexes.keys())} | checking {len(paths_from_sequence_to_longest_object)} paths",
-            end="",
-        )
-
-        weighted_length = 0
-        max_prob = 0
-        max_prob_length = 0
-        for path in paths_from_sequence_to_longest_object:
-            prob = 1
-            length = len(path) - 1
-            for i in range(len(path) - 1):
-                if prob < 0.001:
-                    prob = 0
-                    break
-                prob *= G.get_edge_data(path[i + 1], path[i])["weight"] / sum(
-                    [data["weight"] for _, _, data in G.in_edges(path[i], data=True)]
-                )
-                if prob > max_prob:
-                    max_prob = prob
-                    max_prob_length = length
-
-        disassembly_indexes[sequence] = max_prob_length
-    print(f"\n Averaged DI: {sum(disassembly_indexes.values()) / len(disassembly_indexes.keys()):.2f}")
+    disassembly_indexes = {sequence: [] for sequence in G.nodes()}
+    starting_sequence = sorted(list(disassembly_indexes.keys()), key=len)[-1]
+    sequence = starting_sequence
+    # release particle
+    steps = 0
+    for _ in range(N_particles):
+        while True:
+            disassembly_indexes[sequence].append(steps)
+            out_edges = G.out_edges(sequence, data=True)
+            if len(out_edges) == 0:
+                sequence = starting_sequence
+                steps = 0
+                break
+            weights = np.array([weight["weight"] for _, _, weight in out_edges])
+            sum_weights = sum(weights)
+            weights = [w / sum_weights for w in weights]
+            targets = [target for _, target, _ in out_edges]
+            next_node = np.random.choice(targets, p=weights)
+            sequence = next_node
+            steps = steps + 1
+        
     return disassembly_indexes
+
